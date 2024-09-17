4336e0a0267e5ec9d1d8b1e8dddb81c7647c1cfb
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2024-09-17 17:26:34 +0300

    fetched data and added images

--- src/component/meals/mealsGrid.tsx
+++ src/component/meals/mealsGrid.tsx
@@ -1,21 +1,34 @@
 import classes from "./mealsGrid.module.css";
 import MealItem from "@/component/meals/mealsItem";
 
+type Meal = {
+  id: number;
+  slug: string;
+  title: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+};
 
-export default function MealsGrid({ meals }: any) {
+type MealsGridProps = {
+  meals: Meal[];  // Define the structure of the props
+};
+
+export default function MealsGrid({ meals }: MealsGridProps) {
   return (
     <ul className={classes.meals}>
-      {meals.map((meal: { id: null | undefined; }) => ((
+      {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem
-            title={""}
-            slug={""}
-            image={""}
-            summary={""}
-            creator={""}
-            {...meals} />
-        </li>)))}
+            title={meal.title}
+            slug={meal.slug}
+            image={meal.image}
+            summary={meal.summary}
+            creator={meal.creator}
+          />
+        </li>
+      ))}
     </ul>
-  )
-
+  );
 }
