8d4e623dddcc01789c3d03408e7d819a84849048
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2024-09-18 19:58:29 +0300

    image picker

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound(){
   return <main className="not-found">
-    <h1>Not found</h1>
-    <p>We couldn't find the requested page.</p>
+    <h1>Meal not found</h1>
+    <p>We couldn't find the requested meal page.</p>
   </main>
 }
