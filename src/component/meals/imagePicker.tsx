6484758d548f6baf8de189d50183270c340b970e
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2024-09-19 12:46:56 +0300

    added image picker preview

--- src/component/meals/imagePicker.tsx
+++ src/component/meals/imagePicker.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import Image from 'next/image';
+
 
 import classes from './imagePicker.module.css';
 
@@ -11,6 +13,7 @@ type Props = {
 
 export default function ImagePicker({ label, name }: Props) {
   const imageInput = useRef<HTMLInputElement | null>(null);
+  const [pickedImage, setPickedImage] = useState<string | undefined>(undefined)
 
   function handlePickClick() {
     if (imageInput.current) {
@@ -18,10 +21,39 @@ export default function ImagePicker({ label, name }: Props) {
     }
   }
 
+  function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    let file;
+    if (event.target.files) {
+      file = event.target.files[0];
+    }
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      if (fileReader.result !== null) {
+        setPickedImage(fileReader.result as string);
+
+      }
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>{!pickedImage && <p>No image picked yet</p>}
+          {pickedImage &&
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -29,6 +61,8 @@ export default function ImagePicker({ label, name }: Props) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          required
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
